d65469f8d4243f2d5752e3510498365de95d261e
Shreyasingh-2107 <itsmeshreya755@gmail.com>
2025-04-06 16:51:03 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,3 +2,6 @@ import os
 import json
 def save_settings(username, settings):
     """Save user settings"""
+    users_dir = "users"
+    if not os.path.exists(users_dir):
+        os.makedirs(users_dir)
